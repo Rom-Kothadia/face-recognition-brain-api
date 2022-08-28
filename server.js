c63d0feff682fead4d714d2b139678a96b09c248
Rom Kothadia <kothadiarom@gmail.com>
2022-08-27 20:28:55 -0400

    updating database

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-horizontal-21830',
     user : 'postgres',
-    password : 'smart_brain',
+    password : '',
     database : 'smart_brain'
   }
 });
